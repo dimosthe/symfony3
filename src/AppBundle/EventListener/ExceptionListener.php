fe14a4634d497c07bccb3275e356b1654701f3e4
george <george.dimosthenous@hymarkets.com>
2017-03-16 12:05:05 +0200

    return json responses on exceptions

---
+++ src/AppBundle/EventListener/ExceptionListener.php
@@ -0,0 +1,36 @@
+<?php
+// src/AppBundle/EventListener/ExceptionListener.php
+namespace AppBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Symfony\Bridge\Monolog\Logger;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ExceptionListener
+{
+    private $logger;
+
+    public function __construct(Logger $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+        $code = $exception->getStatusCode();
+
+        $responseData = [
+            'error' => [
+                'code' => $code,
+                'message' => $exception->getMessage()
+            ]
+        ];
+
+        $event->setResponse(new JsonResponse($responseData, $code));
+    }
+}
+?>
